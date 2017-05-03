8935c691385b7a5d8027dd770f47eadadf78ffc4
Nick <handnf@gmail.com>
2017-05-02 23:44:47 -0400

    Adds promo graphics

--- app/src/main/java/news/androidtv/bannerpack/DemoActivity.java
+++ app/src/main/java/news/androidtv/bannerpack/DemoActivity.java
@@ -13,5 +13,6 @@ public class DemoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
+        getActionBar().hide();
     }
 }
