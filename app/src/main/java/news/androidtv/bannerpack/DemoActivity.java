65f0cd2bc7a72d0354a2a9224a262c6ded715716
Nick <handnf@gmail.com>
2017-05-02 23:53:16 -0400

    Adds a feature to download app

--- app/src/main/java/news/androidtv/bannerpack/DemoActivity.java
+++ app/src/main/java/news/androidtv/bannerpack/DemoActivity.java
@@ -1,7 +1,12 @@
 package news.androidtv.bannerpack;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.TextView;
 
 /**
  * Created by Nick on 5/2/2017.
@@ -14,5 +19,37 @@ public class DemoActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
         getActionBar().hide();
+
+        // Check if the app is on our device.
+        if (isPackageInstalled("news.androidtv.tvapprepo") || isPackageInstalled("news.androidtv.tvapprepo.playstore")) {
+            // Yes
+            ((TextView) findViewById(R.id.tvapprepo_status)).setText("Tv App Repo is installed");
+        } else {
+            ((TextView) findViewById(R.id.tvapprepo_status)).setText("Tv App Repo is not installed. Click Enter to open Google Play.");
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_A:
+            case KeyEvent.KEYCODE_ENTER:
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+                // Open that app in the store.
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://play.google.com/store/apps/details?id=news.androidtv.tvapprepo.playstore"));
+                startActivity(i);
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private boolean isPackageInstalled(String packagename) {
+        PackageManager packageManager = getPackageManager();
+        try {
+            packageManager.getPackageInfo(packagename, 0);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
     }
 }
